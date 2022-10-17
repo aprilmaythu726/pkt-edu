d443da93fb95a099a786280fa374ec186046d46f
yoonme <yoonmem86@gmail.com>
2022-10-17 11:23:49 +0630

    fix date error and route name

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 // $config['base_url'] = 'http://192.168.0.2/pkt2.0/';
  // $config['base_url'] = 'http://localhost/pkt2.0/';
-$config['base_url'] = 'http://localhost/pkt/';
+$config['base_url'] = 'http://localhost/pkt-edu/';
 
 /*
 |--------------------------------------------------------------------------
