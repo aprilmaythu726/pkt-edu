abcbee65314b076aa66825cce08c4e3a49cca0f7
aprilmaythu726 <aprilmaythu726@gmail.com>
2022-12-02 09:00:17 +0630

    new start

---
+++ application/logs/log-2022-12-02.php
@@ -0,0 +1,43 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-12-02 02:22:19 --> Severity: 8192 --> preg_replace(): Passing null to parameter #3 ($subject) of type array|string is deprecated C:\xampp\htdocs\pkt-edu\application\helpers\custom_helper.php 132
+ERROR - 2022-12-02 02:22:19 --> Severity: 8192 --> strtotime(): Passing null to parameter #1 ($datetime) of type string is deprecated C:\xampp\htdocs\pkt-edu\application\helpers\custom_helper.php 157
+ERROR - 2022-12-02 02:22:19 --> Severity: 8192 --> strtotime(): Passing null to parameter #1 ($datetime) of type string is deprecated C:\xampp\htdocs\pkt-edu\application\helpers\custom_helper.php 157
+ERROR - 2022-12-02 02:22:39 --> Severity: 8192 --> preg_replace(): Passing null to parameter #3 ($subject) of type array|string is deprecated C:\xampp\htdocs\pkt-edu\application\helpers\custom_helper.php 132
+ERROR - 2022-12-02 02:22:39 --> Severity: 8192 --> strtotime(): Passing null to parameter #1 ($datetime) of type string is deprecated C:\xampp\htdocs\pkt-edu\application\helpers\custom_helper.php 157
+ERROR - 2022-12-02 02:22:39 --> Severity: 8192 --> strtotime(): Passing null to parameter #1 ($datetime) of type string is deprecated C:\xampp\htdocs\pkt-edu\application\helpers\custom_helper.php 157
+ERROR - 2022-12-02 02:22:55 --> Severity: 8192 --> preg_replace(): Passing null to parameter #3 ($subject) of type array|string is deprecated C:\xampp\htdocs\pkt-edu\application\helpers\custom_helper.php 132
+ERROR - 2022-12-02 02:22:55 --> Severity: 8192 --> strtotime(): Passing null to parameter #1 ($datetime) of type string is deprecated C:\xampp\htdocs\pkt-edu\application\helpers\custom_helper.php 157
+ERROR - 2022-12-02 02:22:55 --> Severity: 8192 --> strtotime(): Passing null to parameter #1 ($datetime) of type string is deprecated C:\xampp\htdocs\pkt-edu\application\helpers\custom_helper.php 157
+ERROR - 2022-12-02 02:25:29 --> Severity: 8192 --> preg_replace(): Passing null to parameter #3 ($subject) of type array|string is deprecated C:\xampp\htdocs\pkt-edu\application\helpers\custom_helper.php 132
+ERROR - 2022-12-02 02:25:29 --> Severity: 8192 --> strtotime(): Passing null to parameter #1 ($datetime) of type string is deprecated C:\xampp\htdocs\pkt-edu\application\helpers\custom_helper.php 157
+ERROR - 2022-12-02 02:25:29 --> Severity: 8192 --> strtotime(): Passing null to parameter #1 ($datetime) of type string is deprecated C:\xampp\htdocs\pkt-edu\application\helpers\custom_helper.php 157
+ERROR - 2022-12-02 02:27:02 --> Query error: Unknown column 'info_phone' in 'field list' - Invalid query: SELECT `id`, `applicant_name`, `applicant_name_kanji`, `jls_name`, `info_phone`, `address`, `std_email`, `created_at`, `updated_at`
+FROM `OSL_JLS_applicant_info`
+ERROR - 2022-12-02 02:27:05 --> Query error: Unknown column 'info_phone' in 'field list' - Invalid query: SELECT `id`, `applicant_name`, `applicant_name_kanji`, `jls_name`, `info_phone`, `address`, `std_email`, `created_at`, `updated_at`
+FROM `OSL_JLS_applicant_info`
+ERROR - 2022-12-02 03:25:23 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'tomatoferret18_pkt' C:\xampp\htdocs\pkt-edu\system\database\drivers\mysqli\mysqli_driver.php 211
+ERROR - 2022-12-02 03:25:23 --> Unable to connect to the database
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined variable: list C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 172
+ERROR - 2022-12-02 08:56:59 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 172
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$user_id C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 256
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined variable: course C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 258
+ERROR - 2022-12-02 08:56:59 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 258
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$activate_date C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 277
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$activate_date C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 277
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$status C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 289
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$status C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 294
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$user_id C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 256
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined variable: course C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 258
+ERROR - 2022-12-02 08:56:59 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 258
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$activate_date C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 277
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$activate_date C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 277
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$status C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 289
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$status C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 294
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$user_id C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 256
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined variable: course C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 258
+ERROR - 2022-12-02 08:56:59 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 258
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$activate_date C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 277
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$activate_date C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 277
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$status C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 289
+ERROR - 2022-12-02 08:56:59 --> Severity: Notice --> Undefined property: stdClass::$status C:\xampp\htdocs\pkt-edu\application\views\dashboard\langschoolstudent\lists.php 294
